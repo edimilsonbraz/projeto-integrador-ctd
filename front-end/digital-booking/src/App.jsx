5a03e8ba96a9e6855ffc6a0f2795c7c7f25dcb88
Renan Miranda <renanmirandae@gmail.com>
2023-02-15 18:56:22 -0300

    Criando um arquivo css especifico para a pagina de registro

--- front-end/digital-booking/src/App.jsx
+++ front-end/digital-booking/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
-import global from './styles/global.css'
+import global from './styles/global.css';
+import register from './styles/register.css';
 
 export function App() {
   return (
