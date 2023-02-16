7e37bda40c08fd83a7198c25c99edafc2b1ef680
Renan Miranda <renanmirandae@gmail.com>
2023-02-16 15:02:10 -0300

    Criação do bloco de categorias da página home

--- front-end/digital-booking/src/pages/Home/index.jsx
+++ front-end/digital-booking/src/pages/Home/index.jsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import { ContainerCategory } from "../../components/ContainerCategory";
-
-export function Home() {
-  return (
-    <div>
-      <ContainerCategory></ContainerCategory>
-=======
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLocationDot,
@@ -15,31 +8,33 @@ import './style.css'
 
 export function Home() {
   return (
-    <div className="containerBuscador">
-      <h1>Buscar ofertas em hotéis, casas e muito mais</h1>
+    <>
+      <div className="containerBuscador">
+        <h1>Buscar ofertas em hotéis, casas e muito mais</h1>
 
-      <div className="contentInputs">
-        <div className="inputs">
-          <label htmlFor="destino">
-            <FontAwesomeIcon icon={faLocationDot} />
-          </label>
-          <input type="text" id="destino" placeholder="Onde vamos?" />
+        <div className="contentInputs">
+          <div className="inputs">
+            <label htmlFor="destino">
+              <FontAwesomeIcon icon={faLocationDot} />
+            </label>
+            <input type="text" id="destino" placeholder="Onde vamos?" />
+          </div>
+
+          <div className="inputs">
+            <label htmlFor="check-in">
+              <FontAwesomeIcon icon={faCalendarCheck} />
+            </label>
+            <input
+              type="text"
+              id="check-in"
+              placeholder="Check in - Check out"
+            />
+          </div>
+
+          <button>Buscar</button>
         </div>
-
-        <div className="inputs">
-          <label htmlFor="check-in">
-            <FontAwesomeIcon icon={faCalendarCheck} />
-          </label>
-          <input
-            type="text"
-            id="check-in"
-            placeholder="Check in - Check out"
-          />
-        </div>
-
-        <button>Buscar</button>
       </div>
->>>>>>> 0078a0a2e0ab2649630d8823c7412a0e4a3e598d
-    </div>
+      <ContainerCategory></ContainerCategory>
+    </>
   )
 }
