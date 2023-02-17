757d7e7725b7fe616abb869a24f0971fc5b300bb
Renan Miranda <renanmirandae@gmail.com>
2023-02-17 11:29:25 -0300

    Responsividade no bloco de categorias da pagina home

--- front-end/digital-booking/src/components/ContainerCategory/index.jsx
+++ front-end/digital-booking/src/components/ContainerCategory/index.jsx
@@ -1,4 +1,5 @@
 import './style.css';
+import '../../styles/global.css'
 import categories from '../../../categories.json';
 import { Card } from '../Card';
 
@@ -8,15 +9,16 @@ export function ContainerCategory()
     const category = categories.category;
 
     return (
-        <div className="category">
-            <h2>Buscar por tipo de acomodação</h2>
+        <div className='container'>
+            <h1>Buscar por tipo de acomodação</h1>
             <div className='container-card'>
+                {category.map(current =>
                 {
-                    category.map(current =>
-                        {
-                            return <Card img={current.img} title={current.title} number={current.found}></Card>
-                        })
+                    console.log(current.img)
+                    return <Card key={current.title} img={current.img} title={current.title} number={current.found}className='card'>
+                    </Card>
                 }
+                )}
             </div>
         </div>
     );
